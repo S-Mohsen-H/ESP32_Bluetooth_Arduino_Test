a9a67e45b0ee9ca6c001675b8c30e6a75f0994b4
S-Mohsen-H <seyyedmohsenhosainy@gmail.com>
2023-06-06 11:52:19 +0330

    14020316_1151 Start is now called once

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,21 @@
 #include <BluetoothSerial.h>
 
 esp_bd_addr_t *lPeer;
-esp_bd_addr_t tsco = {0xad, 0x8f, 0xe7, 0xb7, 0x60, 0xc1};
-// ad:8f:e7:b7:60:c1
+// esp_bd_addr_t tsco = {0xad, 0x8f, 0xe7, 0xb7, 0x60, 0xc1};
+// esp_bd_addr_t bluM = {0x4d, 0x01, 0xb5, 0x3b, 0xa8, 0x66};
+
+// ad:8f:e7:b7:60:c1  tsco
+// 4d:01:b5:3b:a8:66  bluetooth music
 
 void setup()
 {
   Serial.begin(115200);
   a2dp_source.set_local_name("ESP32 a2dp SOUrce");
   a2dp_source.set_discoverability(ESP_BT_GENERAL_DISCOVERABLE);
+  a2dp_source.start(); // TSCO-TH5365TWS
   a2dp_source.set_volume(30);
+  vTaskDelay(200 / portTICK_PERIOD_MS);
+  // a2dp_source.disconnect();
 }
 
 void loop()
@@ -24,69 +30,60 @@ void loop()
 
   switch (fCommand[0])
   {
-  case 'y':
-  {
-    a2dp_source.connect_to(tsco);
+
+  case 'X':                //    only use if you are not going to use bluetooth in the current boot/session.
+    a2dp_source.end(true); //    if .end() is used, you cannot use bluetooth without rebooting microcontroller
     break;
-  }
-  case 'x':
+  case '6':
+    a2dp_source.reconnect();
+    break;
+
+  case '5':
   {
     lPeer = a2dp_source.get_last_peer_address();
-    memcpy(tsco, *lPeer, sizeof(*lPeer));
+    // memcpy(tsco, *lPeer, sizeof(*lPeer));
 
     const char *s = a2dp_source.to_str(*lPeer);
     // SS = s;
     Serial.println(s);
     break;
   }
-  case 'd':
+  case 'd': // disconnect
 
-    a2dp_source.end(false);
+    a2dp_source.disconnect();
     break;
 
-  case 'l':
-  {
-    a2dp_source.set_ssid_callback(isValid); // Remove SSID Callback
-  }
   case 'L':
     a2dp_source.set_ssid_callback(doNothing); // Remove SSID Callback
     break;
 
-  case 'k':
-
-    Serial.println(a2dp_source.get_connection_state());
+  case 'l':
+    a2dp_source.set_ssid_callback(isValid); // submit SSID Callback
     break;
 
-  case 'j':
-
+  case '4':
     a2dp_source.write_data(data);
     break;
 
-  case 'm':
-
-    a2dp_source.start("TSCO-TH5365TWS"); // TSCO-TH5365TWS
-    a2dp_source.set_volume(30);
-
+  case '3':
+    Serial.println(a2dp_source.get_connection_state());
     break;
 
-  case 'n':
-
-    a2dp_source.start("Bluetooth Music"); // TSCO-TH5365TWS
-    a2dp_source.set_volume(30);
-
+  case '2': //  Bluetooth Music
+  {
+    esp_bd_addr_t bda = {0x4d, 0x01, 0xb5, 0x3b, 0xa8, 0x66};
+    a2dp_source.connect_to(bda);
+    a2dp_source.set_volume(50);
     break;
+  }
 
-  case 'o':
-
-    a2dp_source.start("S530"); // TSCO-TH5365TWS
+  case '1': //  TSCO-TH5365TWS
+  {
+    esp_bd_addr_t bda = {0xad, 0x8f, 0xe7, 0xb7, 0x60, 0xc1};
+    a2dp_source.connect_to(bda);
     a2dp_source.set_volume(30);
-
     break;
-  case 'p':
-    a2dp_source.start(""); // TSCO-TH5365TWS
-    a2dp_source.set_volume(30);
-
-    break;
+  }
 
   case 'v': /////           set volume
   {
@@ -104,17 +101,17 @@ void loop()
     break;
   }
 
-  case '2':
+  case 'H':
     Heap_Information();
     break;
-  case 'b':
+  case 'h':
     Serial.println(heap_caps_get_largest_free_block(MALLOC_CAP_32BIT));
     Serial.println(heap_caps_get_largest_free_block(MALLOC_CAP_8BIT));
 
     Serial.println(heap_caps_get_free_size(MALLOC_CAP_32BIT));
     Serial.println(heap_caps_get_free_size(MALLOC_CAP_8BIT));
-    heap_caps_dump(MALLOC_CAP_8BIT);
-    heap_caps_dump(MALLOC_CAP_32BIT);
+    // heap_caps_dump(MALLOC_CAP_8BIT);
+    // heap_caps_dump(MALLOC_CAP_32BIT);
 
     Serial.println(heap_caps_check_integrity_all(1));
 
